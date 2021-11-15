f99a7b0a996cb6eedb996e521a7d9a2aa1d4ad57
Aleksander Tudruj <aleksanderwt@gmail.com>
2021-11-15 09:20:06 +0100

    added moving and coping constructors tests

--- constructors_test/constructors.cc
+++ constructors_test/constructors.cc
@@ -8,8 +8,9 @@ TriFuzzyNum num2(2.25, 3.25, 1.25);
 constexpr TriFuzzyNum num3(1.25, 2.25, 3.25);
 constinit static TriFuzzyNum num4(1.25, 2.25, 3.25);
 
-int main() {
+void constructors() {
     using std::cout;
+
     cout << num1 << "\n";
     cout << num2 << "\n";
     cout << num3 << "\n";
@@ -21,4 +22,49 @@ int main() {
     assert(Utils::equals(num2, num3));
     assert(Utils::equals(num2, num4));
     assert(Utils::equals(num3, num4));
+
+    Logger::log("TriFuzzyNum real_t constructor seams to work.");
+}
+
+void coping_constructor() {
+    const TriFuzzyNum a(1, 2, 3);
+    TriFuzzyNum b(a);
+
+    assert(Utils::equals(a, b));
+
+    TriFuzzyNum x(4, 5, 6);
+    TriFuzzyNum y(x);
+
+    assert(Utils::equals(x, y));
+
+    constexpr TriFuzzyNum n(4, 5, 6);
+    TriFuzzyNum m(n);
+
+    assert(Utils::equals(n, m));
+
+    constexpr static TriFuzzyNum l(4, 5, 6);
+    TriFuzzyNum k(l);
+
+    assert(Utils::equals(l, k));
+
+    Logger::log("TriFuzzyNum coping constructor seams to work.");
+}
+
+void moving_constructor() {
+    TriFuzzyNum a(TriFuzzyNum(10.0, 20.0, 30.0));
+
+    assert(Utils::equals(a, TriFuzzyNum(10.0, 20.0, 30.0)));
+
+    TriFuzzyNum b(30, 40, 50);
+    TriFuzzyNum c(std::move(b));
+
+    assert(Utils::equals(c, TriFuzzyNum(30, 40, 50)));
+
+    Logger::log("TriFuzzyNum moving constructor seams to work.");
+}
+
+int main() {
+    constructors();
+
+    return 0;
 }
