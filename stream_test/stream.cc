5d79cbd3e54ba97e5a52f4b7ddd2a5d1564ccfeb
Aleksander Tudruj <aleksanderwt@gmail.com>
2021-11-15 10:41:53 +0100

    Added stream test

--- stream_test/stream.cc
+++ stream_test/stream.cc
@@ -13,10 +13,11 @@ int main() {
     constexpr TriFuzzyNum num3(1.25, 2.25, 3.25);
     constinit static TriFuzzyNum num4(1.25, 2.25, 3.25);
 
-    ss << num1 << " " << num2 << " " << num3 << " " << num4 << "\n";
+    ss << num1 << " " << num2 << " " << num3 << " " << num4;
 
     std::string result = ss.str();
-    assert(result == "(1.25, 2.25, 3.25) (1.25, 2.25, 3.25) (1.25, 2.25, 3.25) (1.25, 2.25, 3.25)");
+    std::string expected = "(1.25, 2.25, 3.25) (1.25, 2.25, 3.25) (1.25, 2.25, 3.25) (1.25, 2.25, 3.25)";
+    assert(result.compare(expected) == 0);
 
     return 0;
 }
